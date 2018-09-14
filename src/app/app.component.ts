01211582ff2002abfc4c71ab536025277f455926
Hai Huynh <Hai.Ba.Huynh@mgm-tp.com>
2018-09-15 00:40:05 +0700

    Add Chat UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spring-boot-websocket-chat-application-client';
+  title = 'Spring WebSocket Chat';
+  private serverUrl = 'http://localhost:8080/socket';
+  private stompClient;
+
+  constructor() {
+    this.connect();
+  }
+
+  connect() {
+    let ws = new SockJS(this.serverUrl);
+    this.stompClient = Stomp.over(ws);
+    let that = this;
+    this.stompClient.connect({}, function () {
+      that.stompClient.subscribe("/chat", (message) => {
+        if (message.body) {
+          $(".chat").prepend("<div class='alert alert-secondary flex-wrap'>" + message.body + "</div>");
+        }
+      });
+    });
+  }
+
+  sendMessage(message) {
+    if (message) {
+      this.stompClient.send("/app/send/message", {}, message);
+    }
+    $('#input').val('');
+  }
 }
